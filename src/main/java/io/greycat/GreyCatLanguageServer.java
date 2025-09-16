da65a2370fa8a433db378d89bf87678ab784e67a
François Fouquet <francois.fouquet@datathings.com>
2025-09-16 12:11:11 +0200

    fix lsp server detection

--- src/main/java/io/greycat/GreyCatLanguageServer.java
+++ src/main/java/io/greycat/GreyCatLanguageServer.java
@@ -21,7 +21,6 @@ public class GreyCatLanguageServer extends OSProcessStreamConnectionProvider {
         Path lsp = null;
         String greycatHome = System.getProperty("user.home") + File.separator + ".greycat";
         String projectBasePath = project.getBasePath();
-        boolean legacy_lsp = false;
         if (projectBasePath != null) {
             lsp = Paths.get(projectBasePath, "bin", "greycat-lang");
         }
@@ -30,8 +29,6 @@ public class GreyCatLanguageServer extends OSProcessStreamConnectionProvider {
         }
         if (!lsp.toFile().exists()) {
             lsp = Paths.get(greycatHome, "bin", "greycat-lang");
-        } else {
-            legacy_lsp = true;
         }
         if (lsp.toFile().exists()) {
             lsp_path = lsp.toAbsolutePath().toString();
@@ -61,12 +58,7 @@ public class GreyCatLanguageServer extends OSProcessStreamConnectionProvider {
         if (node_path == null) {
             throw new RuntimeException("GreyCat Lang Server requires a Node.js runtime in PATH");
         }
-        GeneralCommandLine commandLine;
-        if (legacy_lsp) {
-            commandLine = new GeneralCommandLine(node_path, lsp_path, "--stdio");
-        } else {
-            commandLine = new GeneralCommandLine(node_path, lsp_path, "server", "--stdio");
-        }
+        GeneralCommandLine commandLine = new GeneralCommandLine(node_path, lsp_path, "server", "--stdio");
         commandLine.withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE);
         commandLine.withCharset(StandardCharsets.UTF_8);
         commandLine.withWorkDirectory(project.getBasePath());
